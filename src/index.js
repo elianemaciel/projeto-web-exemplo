0f6c235db7500a9e0c94458de87c308446bcfb57
Eliane Faveron <eliane.faveron@squidit.com.br>
2023-04-06 16:32:16 -0300

    initial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 1. Seleciona o elemento div usando a propriedade id
+var app = document.getElementById("app");
+// 2. Cria um novo elemento <p></p> programáticamente
+var p = document.createElement("p");
+// 3. Adiciona conteúdo de texto
+p.textContent = "Olá, Mundo!";
+// 4. Acrescenta o elemento p no elemento div
+app === null || app === void 0 ? void 0 : app.appendChild(p);
